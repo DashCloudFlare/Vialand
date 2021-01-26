8e051a783da40b3cb446912e6a6f53388bdcbcfa
DashCloudFlare <76724079+DashCloudFlare@users.noreply.github.com>
2021-01-26 09:04:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
-	
+	const navLinks = document.querySelectorAll('.nav-links li');	
 	
 	burger.addEventListener('click', () => {
 
 //Toggle_Nav
-		nav.classList.toggle('nav-active');
-		
+		nav.classList.toggle('nav-active');		
 		
 //Animate_Links
 		navLinks.forEach((link, index) => {
@@ -20,5 +18,28 @@ const navSlide = () => {
 	});
    });
 }
-
 navSlide();
+
+
+
+
+
+var tab_selection = document.querySelectorAll('.tphcm-tab, .hanoi-tab');
+var khuvuc_list = document.querySelectorAll('.khuvuc-list');
+
+	tab_selection.forEach(function(tab, tab_index){
+		tab.addEventListener('click', function(){
+							tab_selection.forEach(function(tab){
+											   tab.classList.remove('active');
+											   })
+											   tab.classList.add('active');
+											   khuvuc_list.forEach(function(content, content_index){
+		if(content_index == tab_index) {
+						content.style.display = "block";
+						}
+						else{
+						    content.style.display = "none";
+						    }
+						})
+					})	
+				})	
