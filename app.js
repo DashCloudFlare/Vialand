62cde578316995358f6cba747dad671c5768fa24
DashCloudFlare <76724079+DashCloudFlare@users.noreply.github.com>
2021-02-04 07:17:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -70,3 +70,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+//Image_Slider_Auto
+setInterval(function(){
+			plusSlides(1)
+			}, 5000);
